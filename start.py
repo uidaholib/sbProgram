a645b30373e7deee6e7de10af14722d0ba6c2bfd
Taylor Rogers <taylor.rogers71@gmail.com>
2017-06-20 09:57:41 -0700

    Using pylint and pycodestyle, the style of the script was updated to reflect good practices.

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Initial login script for the ScienceBase MACRO program
+"""Initial login script for the ScienceBase MACRO/PEON program
 
 This module contains the login functions for the user to log in to ScienceBase
 should they need to. It then asks the user whether they are searching
@@ -8,6 +8,8 @@ ScienceBase data and calls the appropriate script.
 
 PEON
 Program for Executive/Ease Of and Nuisance
+Programmatic Executive and Organization/Operations via Network
+Program for Executive Operations via Network
 menial executive task
 menial assignment and computational relief operator
 Macro Architechture for Computational and Reporting/Recording Operations
@@ -15,19 +17,18 @@ Macro Architechture for Computational and Reporting/Recording Operations
 
 
 from pprint import pprint
-import requests
 import json
+import requests
 import pysb
-
-
-
 
 
 sb = pysb.SbSession()
 
 
-
 def questionLogin():
+    """Function asks whether the user wants to log in. If so, it calls login(),
+    if not it skips the login and calls main().
+    """
     print('''
     Would you like to login?
     (Y / N)
@@ -42,10 +43,14 @@ def questionLogin():
     I'm sorry, I didn't understand that.''')
         questionLogin()
 
+
 def login():
+    """Login function used to prompt the user for a username and password for
+    ScienceBase, and using the pysb function loginc() to login to ScienceBase.
+    """
     user = input('Username: ')
     sb.loginc(user)
-    if sb.is_logged_in(): # if user is logged in. Same as if sb.is_logged_in == True
+    if sb.is_logged_in():
         print('''
     Login Successful''')
         main()
@@ -53,7 +58,12 @@ def login():
         print('''
     Could not log you in to ScienceBase''')
 
+
 def main():
+    """The function that is the main fork from which many of the operations of
+    the program can be accessed. It asks if the user wants to search
+    ScienceBase or choose from a variety of other tasks.
+    """
     print('''
     So what do you want to do?
     1. Search ScienceBase
@@ -71,7 +81,8 @@ def main():
     Sorry, I didnt' understand that.''')
         main()
 
-#===============================================================================
+# ===============================================================================
+
 
 if __name__ == '__main__':
     questionLogin()
