95f309a59077b7df798e899e127fa4ef133b8df2
Taylor Rogers <taylor.rogers71@gmail.com>
2017-06-20 11:42:29 -0700

    Finished up with linter changing styles to best practices, and making script universal/unspecialized.

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
-import requests
 import json
+import requests
 import pysb
 import sys
 
@@ -9,12 +9,12 @@ sb = pysb.SbSession()
 
 totalDataCount = 0
 
-#==============================================================================
-#for creating an Excel workbook:
+# ==============================================================================
+# for creating an Excel workbook:
 from pandas import DataFrame
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
-#==============================================================================
+# ==============================================================================
 ChosenFiscalYear = []
 L1Project = []
 L2DataInProject = []
@@ -26,9 +26,15 @@ L7MissingData = []
 L8MissingDataURL = []
 L9Exceptions = []
 L10Exceptions_IDs = []
-#==============================================================================
+# ==============================================================================
+
+
 def main():
-    #clears lists in case they are populated from a previous tally
+    """The starting function for the "tasks" script that clears the lists
+    necessary for parsing and counting data. It asks the user if they want to
+    count or edit data.
+    """
+    # clears lists in case they are populated from a previous tally
     ChosenFiscalYear[:] = []
     L1Project[:] = []
     L2DataInProject[:] = []
@@ -43,25 +49,30 @@ def main():
     print('''
     What task would you like to perform?
     1. Count ScienceBase data
-    2. Edit an item
+    2. Edit an item (Not yet supported)
     ''')
     answer = input('> ').lower()
     if '1' in answer or 'count' in answer or 'data' in answer:
         countSomething()
     elif '2' in answer or 'edit' in answer:
-        import edit_Item
+        import edit_Item # eyekeeper
         edit_Item.main()
     else:
         print('''
     Sorry, I didnt' understand that. Try typing the number or a keyword.''')
         main()
 
+
 def countSomething():
+    """Should the user want to count data, this function asks for what the user
+    would like to count data.
+    """
     print('''
     What do you want to count data in?
     1. A fiscal year
-    2. A project
-    3. A single ScienceBase item
+    2. A project (Not yet supported)
+    3. A single ScienceBase item (Not yet supported)
+    4. Excel Spreadsheet (Not yet supported)
     ''')
     answer = input('> ').lower()
     if '1' in answer or 'fiscal' in answer or 'year' in answer:
@@ -76,11 +87,11 @@ def countSomething():
         countSomething()
 
 
-def projectDataCount(): #finish this later... eyekeeper
-    # I need to make it so that the program asks the folder, and then asks the year
-    # if user doesn't know the year, or, I guess, even the folder, the program
-    # should list all the projects in the folder(s). So the user CAN narrow the
-    # choices, or select from them all.
+def projectDataCount():  # finish this later... eyekeeper
+    # I need to make it so that the program asks the folder, and then asks the
+    # year if user doesn't know the year, or, I guess, even the folder, the
+    # program should list all the projects in the folder(s). So the user CAN
+    # narrow the choices, or select from them all.
     print('''
     Is the project in the NWCSC folder or the SWCSC folder?
     ''')
@@ -95,9 +106,15 @@ def projectDataCount(): #finish this later... eyekeeper
         print('''
     I didn't understand that input. Let's try that again...
               ''')
-        PubSearch()
+        projectDataCount()
+
 
 def yearDataCount():
+    """Function asks what CSC folder the user wants to count data for and uses
+    that answer to set the variable "r" to an array containing the child item
+    ids of the selected folder. It then calls the function chooseFiscalYear()
+    and passes it the "r" variable.
+    """
     print('''
     Which folder would you like to get the data from?
     NWCSC folder or the SWCSC folder?
@@ -115,9 +132,15 @@ def yearDataCount():
               ''')
         yearDataCount()
 
+
 def chooseFiscalYear(r):
-    #print(sb.is_logged_in())
-    pprint(r)
+    """This function prints out the available fiscal years for the chosen CSC
+    and asks the user to pick the one to be parsed and whose data will be
+    counted. After a choice is made, it calls doubleCheck(), passing it "r" and
+    the new variable "folder" that is the selected fiscal year.
+    """
+    # print(sb.is_logged_in()) # Quantico
+    # pprint(r) # Quantico
     print('''
     Here are the fiscal year folders to choose from:
     ''')
@@ -135,43 +158,44 @@ def chooseFiscalYear(r):
         val = int(float(answer))
         f = val-1
         folder = r[f]
-        doubleCheckNWCSC(folder)
+        doubleCheck(r, folder)
 
     except ValueError:
         print("That's not a number. Let's try this again.")
         chooseFiscalYear(r)
 
 
-    #q = sb.get_item(r[0])
-    #pprint(q)
-    #for i in r:
-    #    q = sb.get_child_ids(i)
-    #    pprint(q)
-
-def doubleCheckNWCSC(folder):
+def doubleCheck(old_r, folder):
+    """This function confirms the fiscal year choice from the previous function.
+    If confirmed, it calls the parse.py script to begin parsing the data. If
+    unconfirmed, it calls chooseFiscalYear() again.
+    """
     r = sb.get_item(folder)
-    #pprint(r) #Quantico
+    # pprint(r) #Quantico
     print('''
     Ok, so we're counting the data in '''+r['title']+'''.
 
     Is that right?
-    (Y / N)'''
-    )
-    answer = input('> ')
-    if 'yes' in answer or 'Yes' in answer or 'y' in answer or 'Y' in answer:
-        FYdataCount(r, folder)
-    elif 'no' in answer or 'No' in answer or 'n' in answer or 'N' in answer:
+    (Y / N)
+    ''')
+    answer = input('> ').lower()
+    if 'yes' in answer or 'y' in answer:
+        import parse
+        parse.main()
+    elif 'no' in answer or 'n' in answer:
         print('''
     Ok, let's try this again.''')
-        yearDataCountNWCSC()
+        chooseFiscalYear(old_r)
     else:
         print('''
-    Sorry, I didn't understand that. Try typing 'Yes' or 'No' or the first letter of your answer.''')
-        doubleCheckNWCSC(folder)
+    Sorry, I didn't understand that. Try typing 'Yes' or 'No' or the first''' +
+              ''' letter of your answer.''')
+        doubleCheck(old_r, folder)
+
 
 if __name__ == '__main__':
     main()
-    #yearDataCountNWCSC()
+    # yearDataCountNWCSC()
 
 
 sb.logout()
