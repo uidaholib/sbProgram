a91f53c4451abed54c3a7a41e3ba9cf00db8ac35
Taylor Rogers <taylor.rogers71@gmail.com>
2017-06-26 14:18:34 -0700

    Added excel variables.

--- g.py
+++ g.py
@@ -3,13 +3,27 @@ ScienceBase PEON program.
 
 This module contains empty lists to be populated with all ScienceBase items,
 projects, and Fiscal Years to be accessed from all other modules via "g.items",
-"g.projects", "g.FiscalYears" and g.itemsToBeParsed."""
+"g.projects", "g.FiscalYears" and g.itemsToBeParsed. It also contains the total
+data count, and the variables needed to print data counting info to excel."""
 
 itemsToBeParsed = []
 items = []
 projects = []
 fiscalYears = []
 
+
 totalDataCount = 0
-missingDataIds = []
-exceptionItems = []
+
+# Lists to be printed to Excel:
+
+ID = []
+ObjectType = []
+Name = []
+FiscalYear = []
+Project = []
+DataInProject = []
+DataPerFile = []
+RunningDataTotal = []
+NestedData = []
+MissingData = []
+Exceptions = []
