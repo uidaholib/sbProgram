cec762d919089e0afbc01ef3645178b65392e79c
Taylor Rogers <taylor.rogers71@gmail.com>
2017-06-26 09:36:03 -0700

    created a new module 'g.py' to contain lists of all items, projects, and fiscal years. Can be accessed from any other module.

---
+++ g.py
@@ -0,0 +1,7 @@
+"""This module contains empty lists to be populated with all ScienceBase items,
+projects, and Fiscal Years to be accessed from all other modules via "g.items",
+"g.projects", and "g.FiscalYears"."""
+
+items = []
+projects = []
+FiscalYears = []
