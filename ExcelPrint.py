9bfd524d7b14609d37801cd80c2f783e98b51623
Taylor Rogers <taylor.rogers71@gmail.com>
2017-07-02 16:51:10 -0700

    Added function for clearing memory after each itemToBeParsed item has been parsed.

--- ExcelPrint.py
+++ ExcelPrint.py
@@ -124,3 +124,24 @@ def saveExcel(ChosenFiscalYear, wb):
     Workbook saved as "'''+str(ChosenFiscalYear)+''' Data Metrics.xlsx" in the working directory.''')
         import specialtyTasks_working3
         specialtyTasks_working3.nowWhat()
+
+
+def clearMemory():
+    print("""
+    Would you like to clear the memory of all of the data parsed so far"""+
+    """, or keep all data to be placed into a single Excel spreadsheet """+
+    """later?""")
+    answer2 = input("> ").lower()
+    if 'clear' in answer2 or 'empty' in answer2:
+        g.itemsToBeParsed[:] = []
+        g.items[:] = []
+        g.projects[:] = []
+        g.fiscalYears[:] = []
+        g.onTheFlyParsing[:] = []
+        return
+    elif 'keep' in answer2 or 'save' in answer2:
+        print('''
+    Ok. Memory contents kept.''')
+        return
+    else:
+        clearMemory()
