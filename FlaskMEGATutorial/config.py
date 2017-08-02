dd2a69359023f335bbe7981a0b13428a8f173c09
Taylor Rogers <taylor.rogers71@gmail.com>
2017-08-02 14:29:10 -0700

    Working on Discover Flask youtube tutorials. Very good.

--- FlaskMEGATutorial/config.py
+++ FlaskMEGATutorial/config.py
@@ -1,3 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -6,3 +9,7 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
