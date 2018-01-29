f0b9829fa2003f375d2de2eef71066bcf2fd12eb
Taylor Rogers <taylor.rogers71@gmail.com>
2018-01-29 14:38:19 -0800

    Complete refactor module.

--- sbMACRO_WebApp/DataCounting/gl.py
+++ sbMACRO_WebApp/DataCounting/gl.py
@@ -1,58 +1,40 @@
+# pylint: disable=C0103
 """This module contains variables that need accessed by all modules in the
 ScienceBase MACRO program.
 
 This module contains empty lists to be populated with all ScienceBase items,
-projects, and Fiscal Years to be accessed from all other modules via "gl.items",
-"gl.projects", "gl.FiscalYears" and gl.itemsToBeParsed. It also contains the total
-data count, and the variables needed to print data counting info to excel."""
+projects, and Fiscal Years to be accessed from all other modules via
+"gl.items","gl.projects", "gl.FiscalYears" and gl.items_to_be_parsed. It also
+contains the total data count, and the variables needed to print data counting
+info to excel."""
 
-#itemsToBeParsed = ["4f8c64d2e4b0546c0c397b46", "5006c2c9e4b0abf7ce733f42", "55e4d96be4b05561fa208585", "58111fafe4b0f497e79892f7"]
-#itemsToBeParsed = ["5006c1e5e4b0abf7ce733f3b", "5006c2c9e4b0abf7ce733f42"]
-itemsToBeParsed = []
+items_to_be_parsed = []
 items = []
-#projects = ["5006e99ee4b0abf7ce733f58", "55ae7b23e4b066a249242391", "5006e94ee4b0abf7ce733f56"]
 projects = []
 fiscalYears = []
-onTheFlyParsing = []
+on_the_fly_parsing = []
 
 totalDataCount = 0
 totalFYData = 0
 
-Current_Item = None
-# # Lists to be printed to Excel:
-# # Project sheet:
+current_item = None
+
 ID = []  # Added
 URL = []
-ObjectType = []  # Added
-Name = []  # Added
-FiscalYear = []  # Added
-Project = []  # Added
-DataInProject = []   # Added
-DataPerFile = []   # Added
-totalFYDataList = []
-RunningDataTotal = []  # Added
+object_type = []  # Added
+name = []  # Added
+fiscal_year = []  # Added
+project = []  # Added
+data_in_project = []   # Added
+data_per_file = []   # Added
+total_fy_data_list = []
+running_data_total = []  # Added
 
-
-
-ProjFiles = {}
-ProjItems = {}
-# For debugging:
-# ID = ['1', '2', '3', '4']  # Added
-# ObjectType = ['Project1', 'Project2', 'Project3', 'Project4']  # Added
-# Name = ['Name1', 'Name2', 'Name3', 'Name4']  # Added
-# FiscalYear = ['2020', '2020', '2020', '2020']  # Added
-# Project = ['Project Name1', 'Project Name2',
-#            'Project Name3', 'Project Name4']  # Added
-# DataInProject = ['1', '2', '3', '4']   # Added
-# DataPerFile = ["['2023', '39930', '9938']",
-#                "['3536', '5479', '57221']", "['5682', '3984368930', '94658938']", "['34633', '3975437930']"]   # Added
-# totalFYDataList = ['939239549', '939239549', '939239549', '939239549']
-# RunningDataTotal = ['9284054281', '9284054282',
-#                     '9284054283', '9284054284']  # Added
-
+project_files = {}
+project_items = {}
 
 # Other sheets
-MissingData = []
-Exceptions = []
+missing_data = []
+exceptions = []
 
 Excel_choice = None
