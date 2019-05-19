16d2914fa78d449167fa28a1f9cfd0c2dc3953ea
Sandeep <sandeepgr.ravi@gmail.com>
2019-05-18 21:42:59 -0500

    Update to sbmacro to fix login sanitization validation registration and password reset bugs

--- sbMACROv2.0/app/auth/forms.py
+++ sbMACROv2.0/app/auth/forms.py
@@ -1,7 +1,7 @@
 """Authentification-related form classes."""
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import Form, StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, Regexp
 from app.models import User
 
 
@@ -17,9 +17,11 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     """Define Registration form and validation criteria for username/email."""
 
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=6, max=32), Regexp('^\w+$', message="Username must contain only letters numbers or underscore")])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=8, max=32), Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$', message="Should contain atleast 1 Lowercase, UpperCase, Digit and SpecialSymbol ")])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
