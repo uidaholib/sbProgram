1b7d32d5743c6295c33f743eeb49b550340588d0
sgravi1992 <sandeepgravi1992@gmail.com>
2020-08-21 08:27:36 -0700

    SMTP config moved to file

--- sbMACROv2.0/config.py
+++ sbMACROv2.0/config.py
@@ -1,10 +1,19 @@
 """Module containing the master configuration class for entire application."""
 import os
 from dotenv import load_dotenv
+from configparser import ConfigParser
+
+sbconfig = ConfigParser()
 
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
 load_dotenv(os.path.join(BASEDIR, '.env'))
+
+sbconfig.read(os.path.join(BASEDIR, 'sbmacro.config'))
+
+mailer = sbconfig["mailer"]
 
 
 class Config(object):
@@ -18,12 +27,11 @@ class Config(object):
     # USER_ENABLE_CHANGE_PASSWORD = True
 
     # Email server details:
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_SERVER = mailer['MAIL_SERVER']
+    MAIL_PORT = int(mailer.get('MAIL_PORT', 25))
     # Bolean flag to enable encrypted connections:
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['ad.sbmacro@gmail.com'
-              ]
+    MAIL_USE_TLS = mailer['MAIL_USE_TLS'] is not None
+    MAIL_USERNAME = mailer['MAIL_USERNAME']
+    MAIL_PASSWORD = mailer['MAIL_PASSWORD']
+    ADMINS = ['ad.sbmacro@gmail.com']
     # Must be changed once hosted. Is a list.
